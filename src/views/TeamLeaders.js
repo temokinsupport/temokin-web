49da235ba0ec9f4bd8d7cc41843f5251b2ac2e43
May Altamerano <altameranomay@yahoo.com.ph>
2021-07-01 22:26:53 +0800

    added team leader page

--- src/views/TeamLeaders.js
+++ src/views/TeamLeaders.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import '../assets/styles/teamleaders.scss';
 
 function TeamLeaders() {
   return (
@@ -8,7 +9,12 @@ function TeamLeaders() {
         <Container>
             <Row>
                 <Col>
-                
+                    <h1 className="--title">Meet Our Leaders</h1>
+                    <p className="--desc">With a combined 55 years of experience in the construction and property industry, <br/>
+                    our Board of Directors bring to the table a wide spectrum skill sets and visionary <br/>
+                    ideas will keep the TEMOKIN ship steering in the right direction. Here you'll meet a <br/>
+                    diverse group leaders, from highly-acclaimed and respected veterans to a new <br/>
+                    generation of young powerhouses.</p>
                 </Col>
             </Row>
         </Container>
