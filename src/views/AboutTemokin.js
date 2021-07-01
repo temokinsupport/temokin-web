5aa2031556ff0d109acae74802f5011d1089bbea
May Altamerano <altameranomay@yahoo.com.ph>
2021-07-01 16:26:42 +0800

    modified about page temokin

--- src/views/AboutTemokin.js
+++ src/views/AboutTemokin.js
@@ -36,6 +36,10 @@ function AboutTemokin() {
       <div className="--header-container">
         <Splide
           options={ {
+            type         : 'fade',
+            autoplay     : true,
+            pauseOnHover : false,
+            resetProgress: false,
             height   : '100vh',
             direction: 'ttb',
           } }
@@ -104,6 +108,160 @@ function AboutTemokin() {
               <h1 className="--title">WINS <br/>
               YOUR TRUST <br/>
               EVERY TIME</h1>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="--container-3">
+        <Container>
+          <Row>
+            <Col>
+              <Image src={require('../assets/images/aboutus/About_Logo-02.png').default} className="--img"/>
+              <h2 className="--title">OUR MISSION</h2>
+              <p className="--sub-title">
+                To deliver outstanding work <br/>
+                consistently, punctually and with <br/>
+                absolute integrity.
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="--container-4">
+      </div>
+      <div className="--container-5">
+        <div className="--bg-1"/>
+        <div className="--bg-2"/>
+        <Container>
+          <Row>
+            <Col>
+            <Splide
+              options={ {
+                type         : 'fade',
+                autoplay     : true,
+                pauseOnHover : false,
+                resetProgress: false,
+                height   : '80vh',
+              } }
+            >
+              <SplideSlide >
+                <Image src={require('../assets/images/aboutus/About_IMG-People.png').default}/>
+                <div className="--right-container">
+                  <h5 className="--title">People</h5>
+                  <hr/>
+                  <p className="--desc">We are <br/>
+                  client-oriented</p>
+                </div>
+              </SplideSlide>
+              <SplideSlide >
+                <Image src={require('../assets/images/aboutus/About_IMG-Accoutability.png').default}/>
+                <div className="--right-container">
+                  <h5 className="--title">Accountability</h5>
+                  <hr/>
+                  <p className="--desc">We ensure <br/>
+                  peace of mind</p>
+                </div>
+              </SplideSlide>
+              <SplideSlide >
+                <Image src={require('../assets/images/aboutus/About_IMG-Timing.png').default}/>
+                <div className="--right-container">
+                  <h5 className="--title">Timing</h5>
+                  <hr/>
+                  <p className="--desc">We deliver <br/>
+                  on schedule</p>
+                </div>
+              </SplideSlide>
+              <SplideSlide >
+                <Image src={require('../assets/images/aboutus/About_IMG-Quality.png').default}/>
+                <div className="--right-container">
+                  <h5 className="--title">Quality</h5>
+                  <hr/>
+                  <p className="--desc">We exceed <br/>
+                  Expectations</p>
+                </div>
+              </SplideSlide>
+            </Splide>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="--container-6"></div>
+      <div className="--container-7">
+        <div className="--bg-1"/>
+        <Container>
+          <Row>
+            <Col>
+            <Splide
+              options={ {
+                type         : 'fade',
+                autoplay     : true,
+                pauseOnHover : false,
+                resetProgress: false,
+                height   : '80vh',
+              } }
+            >
+              <SplideSlide >
+                <Image src={require('../assets/images/aboutus/About_IMG-People.png').default}/>
+                <div className="--right-container">
+                  <h5 className="--title">Wisdom</h5>
+                  <hr/>
+                  <p className="--desc">We are relentless in our <br/>
+                  pursuit for wisdom</p>
+                </div>
+              </SplideSlide>
+              <SplideSlide >
+                <Image src={require('../assets/images/aboutus/About_IMG-Equality.png').default}/>
+                <div className="--right-container">
+                  <h5 className="--title">Equality</h5>
+                  <hr/>
+                  <p className="--desc">We ensure <br/>
+                  peace of mind</p>
+                </div>
+              </SplideSlide>
+              <SplideSlide >
+                <Image src={require('../assets/images/aboutus/About_IMG-Integrity.png').default}/>
+                <div className="--right-container">
+                  <h5 className="--title">Integrity</h5>
+                  <hr/>
+                  <p className="--desc">We deliver <br/>
+                  on schedule</p>
+                </div>
+              </SplideSlide>
+              <SplideSlide >
+                <Image src={require('../assets/images/aboutus/About_IMG-Diligence.png').default}/>
+                <div className="--right-container">
+                  <h5 className="--title">Diligence</h5>
+                  <hr/>
+                  <p className="--desc">We exceed <br/>
+                  Expectations</p>
+                </div>
+              </SplideSlide>
+              <SplideSlide >
+                <Image src={require('../assets/images/aboutus/About_IMG-Guidance.png').default}/>
+                <div className="--right-container">
+                  <h5 className="--title">Guidance</h5>
+                  <hr/>
+                  <p className="--desc">We exceed <br/>
+                  Expectations</p>
+                </div>
+              </SplideSlide>
+            </Splide>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="--container-8">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="--title">We Dig</h1>
+              <p className="--desc">More than just our values, we dig is a tribute <br/>
+              to our early pipe-jacking and earthworks days,<br/>
+              and it serves as a constant reminder of how <br/>
+              even the toughest journeys can lead to the <br/>
+              most rewarding of destinations. and trust us, <br/>
+              digging is as tough as it gets.
+              </p>
             </Col>
           </Row>
         </Container>
