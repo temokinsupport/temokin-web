a8f65a52b12c7d9942925fc3387020dd47efc8b7
May Altamerano <altameranomay@yahoo.com.ph>
2021-07-02 20:16:42 +0800

    added property development page

--- src/views/PorpertyDevelopment.js
+++ src/views/PorpertyDevelopment.js
@@ -5,7 +5,19 @@ import '../assets/styles/propertydevelopment.scss';
 function PorpertyDevelopment() {
   return (
     <div className="--property-development">
-      <h1>Porperty Development</h1>
+      <div className="--bg-1">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="--title">Property Development</h1>
+              <p className="--desc">With over 50 years of combined experience in development, we have designed <br/>
+              and built a multitude of complex skycrapers, hospitals, modern retail spaces <br/>
+              nad highquality homes, with our crowning glory being the construction of <br/>
+              Istana Negara, the National Palace of Malaysia.</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
