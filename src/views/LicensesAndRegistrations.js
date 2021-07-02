f320799fa1cfd2e5bb45e751db42bc3a6e81fd82
May Altamerano <altameranomay@yahoo.com.ph>
2021-07-02 15:48:29 +0800

    update licenses and registrations page

--- src/views/LicensesAndRegistrations.js
+++ src/views/LicensesAndRegistrations.js
@@ -69,6 +69,83 @@ function LicensesAndRegistrations() {
                 </Row>
             </Container>
         </div>
+        <div className="--bg-3">
+            <Container className="--max">
+                <Row>
+                    <Col>
+                        <h1 className="--title"> Quality Assurance</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={7}>
+                        <Carousel fade>
+                            <Carousel.Item>
+                                <Image src={require('../assets/images/licensesandregistrations/Licenses_IMG-QA-01.png').default}/>
+                                <Carousel.Caption>
+                                    <h3 className="--caption"> ISO 9001 : 2008<br/>
+                                    Sirim Qas </h3>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <Image src={require('../assets/images/licensesandregistrations/Licenses_IMG-QA-02.png').default}/>
+                                <Carousel.Caption>
+                                    <h3 className="--caption"> ISO 9001 : 2008<br/>
+                                    Sirim Qas </h3>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <Image src={require('../assets/images/licensesandregistrations/Licenses_IMG-QA-03.png').default}/>
+                                <Carousel.Caption>
+                                    <h3 className="--caption"> ISO 9001 : 2008<br/>
+                                    Sirim Qas </h3>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <Image src={require('../assets/images/licensesandregistrations/Licenses_IMG-QA-04.png').default}/>
+                                <Carousel.Caption>
+                                    <h3 className="--caption"> ISO 9001 : 2008<br/>
+                                    Sirim Qas </h3>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <Image src={require('../assets/images/licensesandregistrations/Licenses_IMG-QA-05.png').default}/>
+                                <Carousel.Caption>
+                                    <h3 className="--caption"> ISO 9001 : 2008<br/>
+                                    Sirim Qas </h3>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <Image src={require('../assets/images/licensesandregistrations/Licenses_IMG-QA-06.png').default}/>
+                                <Carousel.Caption>
+                                    <h3 className="--caption"> ISO 9001 : 2008<br/>
+                                    Sirim Qas </h3>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <Image src={require('../assets/images/licensesandregistrations/Licenses_IMG-QA-07.png').default}/>
+                                <Carousel.Caption>
+                                    <h3 className="--caption"> ISO 9001 : 2008<br/>
+                                    Sirim Qas </h3>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        </Carousel>
+                    </Col>
+                    <Col>
+                        <div className="--text-container">
+                            <hr/>
+                            <p className="--desc"> We maintain a quality management <br/>
+                            systme which fulfils the <br/>
+                            requirements of iso <br/>
+                            9001 : 2008, specifically for <br/>
+                            roadworks and infrustructure <br/>
+                            construction, certified by sirim <br/>
+                            qas international certification <br/>
+                            network, iqnet. </p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     </div>
   );
 }
