b9de73019c6a62fe90fef0404a3ff4b77470dbbb
May Altamerano <altameranomay@yahoo.com.ph>
2021-07-02 16:01:34 +0800

    update civil engineering page

--- src/views/CivilEngineering.js
+++ src/views/CivilEngineering.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import '../assets/styles/civilengineering.scss';
 
 function CivilEngineering() {
   return (
     <div className="--civil-engineering">
-      <h1>Civil Engineering</h1>
+      <div className="--bg-1">
+        <Container>
+          <Row>
+            <Col>
+              {/* <h1>Civil Engineering</h1> */}
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
