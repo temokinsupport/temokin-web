1408f9ee1a00206c55a3c4f321e0b19305417c21
May Altamerano <altameranomay@yahoo.com.ph>
2021-07-02 20:05:17 +0800

    update civil engineering styles

--- src/views/PorpertyDevelopment.js
+++ src/views/PorpertyDevelopment.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import '../assets/styles/propertydevelopment.scss';
 
 function PorpertyDevelopment() {
   return (
