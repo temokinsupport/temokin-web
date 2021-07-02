2e9a903b917580ef2b5574cfd83535dedaffb155
May Altamerano <altameranomay@yahoo.com.ph>
2021-07-02 18:54:46 +0800

    update civil engineering page

--- src/views/CivilEngineering.js
+++ src/views/CivilEngineering.js
@@ -184,6 +184,257 @@ function CivilEngineering() {
           </Row>
         </Container>
       </div>
+      <div className="--bg-4">
+        <Container className="--max">
+          <Row>
+            <Col md={6}>
+              <Card>
+                <Card.Img variant="top" src={require('../assets/images/civilengineering/CivilEng_Icon-01.png').default} />
+                <Card.Body>
+                  <Card.Title>
+                    Pipe Jacking Ulu Tiram, <br/>
+                    Johor Bahru For Water <br/>
+                    Privatization @ 1500MM <br/>
+                    DIA. & 1350 DIA.
+                  </Card.Title>
+                  <Card.Text>
+                    <span className="--title">Client</span>
+                    <span>Active Review Sdn Bhd</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Value</span>
+                    <span>RM10 Million</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Period</span>
+                    <span>May 1994 to Oct 1994</span>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={6}>
+              <Card>
+                <Card.Img variant="top" src={require('../assets/images/civilengineering/CivilEng_Icon-02.png').default} />
+                <Card.Body>
+                  <Card.Title>
+                    KTM Line Railway N. Sembilan @ Size Id <br/>
+                    1.5M & Id= 1.8M
+                  </Card.Title>
+                  <Card.Text>
+                    <span className="--title">Client</span>
+                    <span>Active Review Sdn Bhd</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Value</span>
+                    <span>RM10 Million</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Period</span>
+                    <span>May 1994 to Oct 1994</span>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <Card>
+                <Card.Img variant="top" src={require('../assets/images/civilengineering/CivilEng_Icon-01.png').default} />
+                <Card.Body>
+                  <Card.Title>
+                    KTM Line Railway Alor Star, <br/>
+                    @ Size Id= 1.8M
+                  </Card.Title>
+                  <Card.Text>
+                    <span className="--title">Client</span>
+                    <span>Low Then & Sons Sdn Bhd</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Value</span>
+                    <span>RM7.5 Million</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Period</span>
+                    <span>May 1994 to May 1994</span>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={6}>
+              <Card>
+                <Card.Img variant="top" src={require('../assets/images/civilengineering/CivilEng_Icon-02.png').default} />
+                <Card.Body>
+                  <Card.Title>
+                    Associated Works Ktmb, Nilai , <br/>
+                    Neg. Sembilan @ 1.9M
+                  </Card.Title>
+                  <Card.Text>
+                    <span className="--title">Client</span>
+                    <span>Arab Malaysian Development Berhad</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Value</span>
+                    <span>RM2 Million</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Period</span>
+                    <span>Sept 1992 to Dec 1993</span>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <Card>
+                <Card.Img variant="top" src={require('../assets/images/civilengineering/CivilEng_Icon-01.png').default} />
+                <Card.Body>
+                  <Card.Title>
+                    Senay Highway Twin @ Size 1.8M <br/>
+                    DIA.
+                  </Card.Title>
+                  <Card.Text>
+                    <span className="--title">Client</span>
+                    <span>Bina Baik Sdn Bhd</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Value</span>
+                    <span>RM6 Million</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Period</span>
+                    <span>Maarch 1993 to DEc 1993</span>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={6}>
+              <Card>
+                <Card.Img variant="top" src={require('../assets/images/civilengineering/CivilEng_Icon-02.png').default} />
+                <Card.Body>
+                  <Card.Title>
+                    Line Railway Gasing Indah Petaling <br/>
+                    Jaya
+                  </Card.Title>
+                  <Card.Text>
+                    <span className="--title">Client</span>
+                    <span>Golden Hope Berhad</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Value</span>
+                    <span>RM4 Million</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Period</span>
+                    <span>Oct 1992 to Jan 1995</span>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <Card>
+                <Card.Img variant="top" src={require('../assets/images/civilengineering/CivilEng_Icon-03.png').default} />
+                <Card.Body>
+                  <Card.Title>
+                    R.C Bridge 50M Span lot PT <br/>
+                    60, Mukim Pagoh, Alor Gajah <br/>
+                    Melaka
+                  </Card.Title>
+                  <Card.Text>
+                    <span className="--title">Client</span>
+                    <span>JKR Melaka</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Value</span>
+                    <span>RM12 Million</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Period</span>
+                    <span>1996 to 1997</span>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={6}>
+              <Card>
+                <Card.Img variant="top" src={require('../assets/images/civilengineering/CivilEng_Icon-04.png').default} />
+                <Card.Body>
+                  <Card.Title>
+                    Danger Area, Road Works <br/>
+                    Contract No. LKR/NS/P/R/104 @ <br/>
+                    KM32.1 - KM33.4 Of JLN Seremban <br/>
+                    Tampin, Rembau, N. Sembilan
+                  </Card.Title>
+                  <Card.Text>
+                    <span className="--title">Client</span>
+                    <span>JKR Negeri Sembilan</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Value</span>
+                    <span>RM14.8 Million</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Period</span>
+                    <span>Feb 1994 to Nov 1994</span>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <Card>
+                <Card.Img variant="top" src={require('../assets/images/civilengineering/CivilEng_Icon-05.png').default} />
+                <Card.Body>
+                  <Card.Title>
+                    Completion of Sewerage <br/>
+                    Line- Rancangan Jaya <br/>
+                    Selatan, PJS2-PKNS
+                  </Card.Title>
+                  <Card.Text>
+                    <span className="--title">Client</span>
+                    <span>PKNS</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Value</span>
+                    <span>---</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Period</span>
+                    <span>Aug 1992 to July 1993</span>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={6}>
+              <Card>
+                <Card.Img variant="top" src={require('../assets/images/civilengineering/CivilEng_Icon-06.png').default} />
+                <Card.Body>
+                  <Card.Title>
+                    Grade Separated Interchange and <br/>
+                    Associate Works, Tmn Mount Austin, <br/>
+                    Johor Bahru- Kota Tinggi Road
+                  </Card.Title>
+                  <Card.Text>
+                    <span className="--title">Client</span>
+                    <span>JKR Johor</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Value</span>
+                    <span>RM13.42 Million</span>
+                  </Card.Text>
+                  <Card.Text>
+                    <span className="--title">Period</span>
+                    <span>1993 to 1995</span>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
