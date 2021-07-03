1f3aa39e282938a7cbd44be6ebc7636f640f0fbd
May Altamerano <altameranomay@yahoo.com.ph>
2021-07-04 01:00:31 +0800

    added iframe plugin

--- src/views/ContactUs.js
+++ src/views/ContactUs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col, Image, Form, Button } from 'react-bootstrap';
+import Iframe from 'react-iframe';
 import '../assets/styles/contactus.scss';
 
 function ContactUs() {
@@ -82,10 +83,11 @@ function ContactUs() {
             </Col>
             <Col md={6}>
               <div className="--map-container">
-                <iframe 
+                <Iframe 
                 width="100%" 
                 height="100%" 
-                frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Temokin%20Holdings%20Sdn%20Bhd,%20No,2,%20Jalan%2015/48a,%20Sentul%20Raya%20Boulevard,%2051000%20Kuala%20Lumpur,%20Federal%20Territory%20of%20Kuala%20Lumpur,%20Malaysia+(temokin%20holdings%20sdn%20bhd)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+                frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 
+                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Temokin%20Holdings%20Sdn%20Bhd,%20No,2,%20Jalan%2015/48a,%20Sentul%20Raya%20Boulevard,%2051000%20Kuala%20Lumpur,%20Federal%20Territory%20of%20Kuala%20Lumpur,%20Malaysia+(temokin%20holdings%20sdn%20bhd)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"/>
               </div>
             </Col>
           </Row>
