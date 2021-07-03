f7863e7439c792b740f81b2920b15b1a75c1c352
May Altamerano <altameranomay@yahoo.com.ph>
2021-07-03 23:37:28 +0800

    modified contact us page

--- src/views/ContactUs.js
+++ src/views/ContactUs.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import '../assets/styles/contactus.scss';
 
 function ContactUs() {
   return (
     <div className="--contact-us">
-      <h1>Contact Us</h1>
+      <div className="--bg-1">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="--title">Get In Touch</h1>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
