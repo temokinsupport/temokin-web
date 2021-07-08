b0c835117cdc1cbd922b14ce9114b1ef0be53ed3
May Altamerano <altameranomay@yahoo.com.ph>
2021-07-09 00:47:34 +0800

    modified about page and teamleaders

--- src/views/AboutTemokin.js
+++ src/views/AboutTemokin.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import React, { useRef, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { Container, Row, Col, Image, Carousel, Button } from 'react-bootstrap';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '../assets/styles/aboutus.scss';
 import '@splidejs/splide/dist/css/themes/splide-default.min.css';
 
 
 function AboutTemokin() {
+
+  const isTablet = useMediaQuery({ query: '(min-width: 992px)' });
+  const isMobile = useMediaQuery({ query: '(max-width: 992px)' });
 
   const buildProjectLists = [
     {
@@ -28,21 +32,39 @@ function AboutTemokin() {
       img:require('../assets/images/aboutus/About_Icon-TimelyClose.png').default,
       caption:"A Timely Close Out"
     }
-  ]
+  ];
+
+  // MOBILE CAROUSEL
+  const ref = useRef(null);
+  const onPrevClick = () => {
+    ref.current.prev();
+    setRightActive(!isRightActive);
+    setLeftActive(!isLeftActive);
+  };
+  const onNextClick = () => {
+    ref.current.next();
+    setRightActive(!isRightActive);
+    setLeftActive(!isLeftActive);
+  };
+
+  // TOGGLE CLASS
+  const [isRightActive, setRightActive] = useState(false);
+  const [isLeftActive, setLeftActive] = useState(false);
   
   return (
     <div className="--about">
       <div className="--header-container">
-        <Splide
-          options={ {
-            type         : 'fade',
-            autoplay     : true,
-            pauseOnHover : false,
-            resetProgress: false,
-            height   : '100vh',
-            direction: 'ttb',
-          } }
-        >
+        { isTablet &&
+          <Splide
+            options={ {
+              type         : 'fade',
+              autoplay     : true,
+              pauseOnHover : false,
+              resetProgress: false,
+              height   : '100vh',
+              direction: 'ttb',
+            } }
+          >
             <SplideSlide>
               <div className="--about-content">
                 <Container className="--max">
@@ -95,7 +117,55 @@ function AboutTemokin() {
                 </Container>
               </div>
             </SplideSlide>
-        </Splide>
+          </Splide>
+        }
+        { isMobile &&
+          <div className="--carousel-container">
+            <div className="--btn-carousel-container">
+              <Button 
+                variant="light" 
+                onClick={onPrevClick}
+                className={isRightActive ? 'carousel-control-prev': "carousel-control-prev hide"} 
+              />
+              <Button 
+                variant="light" 
+                onClick={onNextClick}
+                className={isLeftActive ? 'carousel-control-next hide': "carousel-control-next"} 
+              />
+            </div>
+                
+            <Carousel fade className="--carousel-mb" ref={ref} loop={false}>
+              <Carousel.Item>
+                <Carousel.Caption>
+                  <h3>Our <br/> Story</h3>
+                  <p>They say every great story has a humble <br/>
+                      beginning, and ours is no exception. <br/>
+                      established in 1989, we started our journey as <br/>
+                      a specialist contractor for highquality <br/>
+                      pipe jacking and earthworks, making our <br/>
+                      first prominent mark in 1991 by delivering <br/>
+                      malaysia's first ever micro-tunneling <br/>
+                      project.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <Carousel.Caption>
+                <h3>Our <br/> Story</h3>
+                  <p>We then quickly spread our wings and grew to become <br/>
+                      the preeminent turkey civil engineering and property <br/>
+                      conglomerate that the nation and the world have come <br/>
+                      to know and  trust today. <br/><br/>
+                      At our heart, we are a construction company with <br/>
+                      an integrated engineering division, which allows us to <br/>
+                      undertake any design-and-build projects with.
+                  </p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+        }
+        
       </div>
       <div className="--container-2">
         <Container>
@@ -126,66 +196,142 @@ function AboutTemokin() {
           </Row>
         </Container>
       </div>
-      <div className="--container-4">
-      </div>
+      { isTablet &&
+        <div className="--container-4">
+        </div>
+      }
+      
+      { isTablet &&
       <div className="--container-5">
-        <div className="--bg-1"/>
-        <div className="--bg-2"/>
-        <Container className="--max">
-          <Row>
-            <Col>
-            <Splide
-              options={ {
-                type         : 'fade',
-                autoplay     : true,
-                pauseOnHover : false,
-                resetProgress: false,
-                height   : '80vh',
-              } }
-            >
-              <SplideSlide >
-                <Image src={require('../assets/images/aboutus/About_IMG-People.png').default}/>
-                <div className="--right-container">
-                  <h5 className="--title">People</h5>
-                  <hr/>
-                  <p className="--desc">We are <br/>
-                  client-oriented</p>
-                </div>
-              </SplideSlide>
-              <SplideSlide >
-                <Image src={require('../assets/images/aboutus/About_IMG-Accoutability.png').default}/>
-                <div className="--right-container">
-                  <h5 className="--title">Accountability</h5>
-                  <hr/>
-                  <p className="--desc">We ensure <br/>
-                  peace of mind</p>
-                </div>
-              </SplideSlide>
-              <SplideSlide >
-                <Image src={require('../assets/images/aboutus/About_IMG-Timing.png').default}/>
-                <div className="--right-container">
-                  <h5 className="--title">Timing</h5>
-                  <hr/>
-                  <p className="--desc">We deliver <br/>
-                  on schedule</p>
-                </div>
-              </SplideSlide>
-              <SplideSlide >
-                <Image src={require('../assets/images/aboutus/About_IMG-Quality.png').default}/>
-                <div className="--right-container">
-                  <h5 className="--title">Quality</h5>
-                  <hr/>
-                  <p className="--desc">We exceed <br/>
-                  Expectations</p>
-                </div>
-              </SplideSlide>
-            </Splide>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-      <div className="--container-6"></div>
-      <div className="--container-7">
+        
+          <div className="--bg-1"/>
+          <div className="--bg-2"/>
+        
+          <Container className="--max">
+            <Row>
+              <Col>
+                <Splide
+                  options={ {
+                    type         : 'fade',
+                    autoplay     : true,
+                    pauseOnHover : false,
+                    resetProgress: false,
+                    height   : '80vh',
+                  } }
+                >
+                  <SplideSlide >
+                    <Image src={require('../assets/images/aboutus/About_IMG-People.png').default}/>
+                    <div className="--right-container">
+                      <h5 className="--title">People</h5>
+                      <hr/>
+                      <p className="--desc">We are <br/>
+                      client-oriented</p>
+                    </div>
+                  </SplideSlide>
+                  <SplideSlide >
+                    <Image src={require('../assets/images/aboutus/About_IMG-Accoutability.png').default}/>
+                    <div className="--right-container">
+                      <h5 className="--title">Accountability</h5>
+                      <hr/>
+                      <p className="--desc">We ensure <br/>
+                      peace of mind</p>
+                    </div>
+                  </SplideSlide>
+                  <SplideSlide >
+                    <Image src={require('../assets/images/aboutus/About_IMG-Timing.png').default}/>
+                    <div className="--right-container">
+                      <h5 className="--title">Timing</h5>
+                      <hr/>
+                      <p className="--desc">We deliver <br/>
+                      on schedule</p>
+                    </div>
+                  </SplideSlide>
+                  <SplideSlide >
+                    <Image src={require('../assets/images/aboutus/About_IMG-Quality.png').default}/>
+                    <div className="--right-container">
+                      <h5 className="--title">Quality</h5>
+                      <hr/>
+                      <p className="--desc">We exceed <br/>
+                      Expectations</p>
+                    </div>
+                  </SplideSlide>
+                </Splide>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      }
+      { isMobile &&
+        <div className="--container-5">
+          <Container>
+            <Row>
+              <Col>
+                <h2 className="--title">Our Operating <br/>
+                Principles</h2>
+              </Col>
+            </Row>
+          </Container>
+          <Container>
+            <Row>
+              <Col>
+                <Splide
+                  options={ {
+                    type         : 'fade',
+                    autoplay     : true,
+                    pauseOnHover : false,
+                    resetProgress: false,
+                  } }
+                >
+                  <SplideSlide >
+                    <Image src={require('../assets/images/aboutus/About_IMG-People.png').default}/>
+                    <div className="--right-container">
+                      <h5 className="--title">People</h5>
+                      <hr/>
+                      <p className="--desc">We are <br/>
+                      client-oriented</p>
+                    </div>
+                  </SplideSlide>
+                  <SplideSlide >
+                    <Image src={require('../assets/images/aboutus/About_IMG-Accoutability.png').default}/>
+                    <div className="--right-container">
+                      <h5 className="--title">Accountability</h5>
+                      <hr/>
+                      <p className="--desc">We ensure <br/>
+                      peace of mind</p>
+                    </div>
+                  </SplideSlide>
+                  <SplideSlide >
+                    <Image src={require('../assets/images/aboutus/About_IMG-Timing.png').default}/>
+                    <div className="--right-container">
+                      <h5 className="--title">Timing</h5>
+                      <hr/>
+                      <p className="--desc">We deliver <br/>
+                      on schedule</p>
+                    </div>
+                  </SplideSlide>
+                  <SplideSlide >
+                    <Image src={require('../assets/images/aboutus/About_IMG-Quality.png').default}/>
+                    <div className="--right-container">
+                      <h5 className="--title">Quality</h5>
+                      <hr/>
+                      <p className="--desc">We exceed <br/>
+                      Expectations</p>
+                    </div>
+                  </SplideSlide>
+                </Splide>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      }
+
+      { isTablet &&
+        <div className="--container-6"></div>
+      }
+
+      {
+        isTablet &&
+        <div className="--container-7">
         <div className="--bg-1"/>
         <Container className="--max">
           <Row>
@@ -249,6 +395,80 @@ function AboutTemokin() {
           </Row>
         </Container>
       </div>
+      }
+
+      { isMobile &&
+        <div className="--container-7">
+          <Container>
+            <Row>
+              <Col>
+                <h2 className="--title">Our Values</h2>
+              </Col>
+            </Row>
+          </Container>
+          <Container>
+            <Row>
+              <Col>
+              <Splide
+                options={ {
+                  type         : 'fade',
+                  autoplay     : true,
+                  pauseOnHover : false,
+                  resetProgress: false,
+                  height   : '80vh',
+                } }
+              >
+                <SplideSlide >
+                  <Image src={require('../assets/images/aboutus/About_IMG-People.png').default}/>
+                  <div className="--right-container">
+                    <h5 className="--title">Wisdom</h5>
+                    <hr/>
+                    <p className="--desc">We are relentless in our <br/>
+                    pursuit for wisdom</p>
+                  </div>
+                </SplideSlide>
+                <SplideSlide >
+                  <Image src={require('../assets/images/aboutus/About_IMG-Equality.png').default}/>
+                  <div className="--right-container">
+                    <h5 className="--title">Equality</h5>
+                    <hr/>
+                    <p className="--desc">We ensure <br/>
+                    peace of mind</p>
+                  </div>
+                </SplideSlide>
+                <SplideSlide >
+                  <Image src={require('../assets/images/aboutus/About_IMG-Integrity.png').default}/>
+                  <div className="--right-container">
+                    <h5 className="--title">Integrity</h5>
+                    <hr/>
+                    <p className="--desc">We deliver <br/>
+                    on schedule</p>
+                  </div>
+                </SplideSlide>
+                <SplideSlide >
+                  <Image src={require('../assets/images/aboutus/About_IMG-Diligence.png').default}/>
+                  <div className="--right-container">
+                    <h5 className="--title">Diligence</h5>
+                    <hr/>
+                    <p className="--desc">We exceed <br/>
+                    Expectations</p>
+                  </div>
+                </SplideSlide>
+                <SplideSlide >
+                  <Image src={require('../assets/images/aboutus/About_IMG-Guidance.png').default}/>
+                  <div className="--right-container">
+                    <h5 className="--title">Guidance</h5>
+                    <hr/>
+                    <p className="--desc">We exceed <br/>
+                    Expectations</p>
+                  </div>
+                </SplideSlide>
+              </Splide>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      }
       <div className="--container-8">
         <Container>
           <Row>
@@ -264,7 +484,7 @@ function AboutTemokin() {
             </Col>
           </Row>
         </Container>
-      </div>
+       </div>
     </div>
   );
 }
