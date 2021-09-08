8da9fb56545338a850e174fe270c5ff9466fad2a
May Altamerano <altameranomay@yahoo.com.ph>
2021-09-08 15:24:45 +0800

    update about page

--- src/components/About/Mission/index.jsx
+++ src/components/About/Mission/index.jsx
@@ -7,25 +7,25 @@ export default function Mission() {
             <Container>
                 <Row>
                     <Col>
-                        <Image 
-                            data-scroll 
-                            data-scroll-speed="-3" src={require('../../../assets/images/aboutus/About_Logo-02.png').default} className="--img"/>
-                        <h2 
-                            data-scroll 
+                        <Image
+                            data-scroll-speed="-1"
+                            data-scroll src={require('../../../assets/images/aboutus/About_Logo-02.png').default} className="--img" />
+                        <h2
+                            data-scroll
                             data-scroll-speed="-1"
                             className="--title">OUR MISSION</h2>
-                        <p  
-                            data-scroll 
+                        <p
+                            data-scroll
                             data-scroll-speed="-1"
-                        className="--sub-title">
-                            To deliver outstanding work <br/>
-                            consistently, punctually and with <br/>
+                            className="--sub-title">
+                            To deliver outstanding work <br />
+                            consistently, punctually and with <br />
                             absolute integrity.
                         </p>
                     </Col>
                 </Row>
             </Container>
-            <div className="mission-bg"/>
+            <div className="mission-bg" />
         </div>
     )
 }
