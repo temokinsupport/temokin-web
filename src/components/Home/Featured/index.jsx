cba642920fab9549e88b30e4623e589379fc377a
Mdelarosa23 <delarosa.michael09@yahoo.com>
2022-11-23 16:26:26 +0800

    mobile bg key project

--- src/components/Home/Featured/index.jsx
+++ src/components/Home/Featured/index.jsx
@@ -147,11 +147,11 @@ export default function Featured() {
                     <Carousel.Caption>
                       <h3>CONSTRUCTION</h3>
                       <p className="--font-normal">
-                        From its humble beginnings as a high-quality pipe <br />
-                        jacking specialist, Temokin led the way with <br />
-                        Malaysia's first microtunnelling project and has since<br />
-                        expanded into the building of electricity substations, <br />
-                        major underground sewage lines, complex bridges <br />
+                        From its humble beginnings as a high-quality pipe{"\n"}
+                        jacking specialist, Temokin led the way with{"\n"}
+                        Malaysia's first microtunnelling project and has since{"\n"}
+                        expanded into the building of electricity substations,{"\n"}
+                        major underground sewage lines, complex bridges{"\n"}
                         and major highways.
                       </p>
                       <NavLink
@@ -171,7 +171,7 @@ export default function Featured() {
                       </NavLink>
                     </Carousel.Caption>
                   </Carousel.Item>
-                  <Carousel.Item>
+                  {/* <Carousel.Item>
                       <img
                         className="d-block w-50"
                         src={require('../../../assets/images/home/Mask Group 65.jpg').default}
@@ -182,7 +182,7 @@ export default function Featured() {
                         <p> Temokin has a collective property development experience of over 50 years, having designed and built complex skycrapers, hospitals and vast modern retail spaces on behalf of our clients. Our current portfolio includes the building of high-quality duplex homes and the new Istana Negara (Malaysia's national palace).</p>
                         <NavLink to="/property-development" variant="light" className="--btn-check-our-work">CHECK OUT OUR WORK</NavLink>
                       </Carousel.Caption>
-                  </Carousel.Item>
+                  </Carousel.Item> */}
                 </Carousel>
               </div>
             )}
