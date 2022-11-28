bb6c2d0a561d1e5c6d51ba25bd87dec808d68b04
Kyle-Atienza <atienzakylefrancis@gmail.com>
2022-11-28 20:02:25 +0800

    update year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -60,7 +60,7 @@ function Footer() {
           </Col>
           <Col>
             <div className="--right-container">
-              <p>© Temokin 2018</p>
+              <p>© Temokin 2022</p>
               <sub>
                 <NavLink
                   exact
