190955142ea5446593594819f1c2ce28768ae81e
Gabo Pundavela <gpundavela@gmail.com>
2023-03-20 14:04:08 +0800

    text change

--- src/components/Home/Launches/index.jsx
+++ src/components/Home/Launches/index.jsx
@@ -34,24 +34,21 @@ export default function Launches() {
               <p className="--txt">2 & 2.5 Storey Terrace Homes</p>
               <p className="--txt">| 20' x 70' | FREEHOLD</p>
               <p className="--desc">
-                Mahligai offers spacious yet affordable terrace homes with
-                perimeter <br />
-                fencing and 24-hour security, nestled in the growing township of{" "}
-                <br />
-                Nusa Damai with all the features to call it your ideal home.
+                Mahligai offers spacious terrace homes <br />
+                located in the growing township of Nusa Damai, <br />
+                making it an ideal place to call home.
               </p>
               <p className="--desc-mobile">
-                Mahligai offers spacious yet affordable terrace homes <br />
-                with perimeter fencing and 24-hour security, nestled <br />
-                in the growing township of Nusa Damai with all the <br />
-                features to call it your ideal home.
+                Mahligai offers spacious terrace homes <br />
+                located in the growing township of Nusa Damai, <br />
+                making it an ideal place to call home.
               </p>
             </div>
           </Col>
         </Row>
         <Row>
           <Col>
-            <a 
+            <a
               data-scroll
               href="https://www.mahligai.my/"
               target="_blank"
@@ -66,7 +63,7 @@ export default function Launches() {
       {/* <div className="launches-bg"/> */}
       <div className="hero-container">
         <LazyLoadImage
-          src={require("../../../assets/images/home/Home_BG-04.png").default} 
+          src={require("../../../assets/images/home/Home_BG-04.png").default}
           className="launches-bg"
         />
       </div>
